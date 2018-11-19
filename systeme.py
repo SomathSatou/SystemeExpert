73b21a1aec0dd2f1ad9c75e12668e2a7a1dbf6e6
chavau <akoia@outlook.fr>
2018-11-19 14:07:33 +0100

    version finale

--- systeme.py
+++ systeme.py
@@ -193,68 +193,78 @@ def traitementAvantL(monBut): #mon but est de la forme fait(valeur, autre)
     regleTrouver = False # variable pour savoir si on trouve encore une règle
     print(monBut)
     if checkContrainte(monBut) : #si le but est dans les faits alors il est atteind
-       # print("but atteind dans la base de fait")
+        print("but atteind dans la base de fait")
         return 2
     else :
 
         for regles,conditions in BR.items() : #pour chaque regles
-            #print("pour chaque règle")
-            regle = regles.split(",") #on met les conséquences sous forme de liste (avant ->)        
+            print("pour chaque règle")
+            regle = regles.split(",") #on met les conséquences sous forme de liste (avant ->)
+        
             variables = []
             faits = []        
+
             for groupeCondition in conditions:#pour chaque conditions (après ->)
-                #print("pour chaque conditions")
+                print("pour chaque conditions")
                 indiceVariable = 0
                 for condition in groupeCondition: # on sépare les variables des conditions
                     if indiceVariable < len(regle): #si ce sont des variables on stock dans variables
                         variables.append(condition)
-                        #print("liste de variable")
+                        print("liste de variable")
                     else :
                         faits.append(condition) #si ce sont des conditions on stock dans faits
-                        #print("liste de faits a vérifier")
+                        print("liste de faits a vérifier")
                     indiceVariable += 1
+
+
             #il faut voir si les faits correspondent au but
             for elements in faits : #pour chaque règle qui amène a cette conséquence
-                #print("cet ensemble de faits a vérifier")
+                print("cet ensemble de faits a vérifier")
                 print(elements)
                 valider = True
                 for element in elements: # element ressemble a : fait(valeur)
                     #on regarde si chaque condition, elle est vraie dans la base de faits
+
                     if(valider == True) :
                         fait = re.compile(r'(?P<fait>[\!\w]+)\((?P<valeur>[\s\w\-]+(,[\s\w\-]+)*)\)\s*')
                         monFait = re.match(fait,element)   
                         if monFait is not None :
+
                             fait = re.compile(r'(?P<fait>[\!\w]+)\((?P<valeur>[\s\w\-]+(,[\s\w\-]+)*)\)\s*')
                             monFait = re.match(fait,element)   
                             if not(checkContrainte(element)): #s'il n'y est pas on invalide cette règle
-                                #print("pas dans la base")
+                                print("pas dans la base")
                                 valider = False
                             else :
-                                #print("dans la base")
+                                print("dans la base")
+
            
                 if valider == True : # si on a passer toutes les contraintes et qu'elles sont toutes dans la base de faits 
-                    #print("cette règle fonctionne")
+                    print("cette règle fonctionne")
                     # il faut vérifier que le fait n'est pas déjà dans la base
                     indice = 1 #indice pour retrouver les variables
                     for nouveauFait in regle:
                         if nouveauFait in BF :
                             if variables[indice] in BF[nouveauFait] :
                             #si le fait existe deja dans la base
-                                #print("le fait existe déjà")
-                                #print(nouveauFait)
-                                #print(variables[indice])
+                                print("le fait existe déjà")
+                                print(nouveauFait)
+                                print(variables[indice])
                             else : # pour ce fait la valeur n'existe pas encore dans la base
                                 regleTrouver = True
                                 addFait(nouveauFait,variables[indice])
-                                #print("on ajoute le fait" + nouveauFait) 
-                                #print(variables[indice]) 
+                                print("on ajoute le fait" + nouveauFait) 
+                                print(variables[indice]) 
 
                         else : # le fait n'xiste pas encore dans la base
                             regleTrouver = True
                             addFait(nouveauFait,variables[indice])
-                            #print("on ajoute le fait" + nouveauFait)
-                            #print(variables[indice] ) 
-                        indice += 1
+                            print("on ajoute le fait" + nouveauFait)
+                            print(variables[indice] ) 
+
+                        indice += 1           
+        
+
     if regleTrouver == False :
         return 0
     else :
@@ -262,17 +272,106 @@ def traitementAvantL(monBut): #mon but est de la forme fait(valeur, autre)
     return -1
 
 
+
 def chainageAvantP():
-	listeElement = []
-	fait = re.compile(r'(?P<fait>\w+)\((?P<valeur>[\s\w\-]+(,[\s\w\-]+)*)\)\s*;')	
-	but = re.match(fait,entree.get())
-	if but is not None:
-		#chainage arrière stocker les resultats dans listElement
-		print("ça passe")
-	else : 
-		listeElement = [["erreur de syntaxe dans la valeur interrogée",entree.get()]]
-	affichageConseil(listeElement)
-	return 
+    fait = re.compile(r'(?P<fait>[\!\w]+)\((?P<valeur>[\s\w\-]+(,[\s\w\-]+)*)\)\s*;')	
+    but = re.match(fait,entree.get())
+
+    # on definis une base de connaissances pour ne pas affecter notre base de fait statique qu'on réinitialise a la fin
+    if but is not None:     
+        maChaine = entree.get()[:-1] #pour enlever le ;
+        affichageConseil(traitementAvantP(maChaine))
+    else :
+        affichageConseil(-1)
+
+    BF = BC # on remet la base de fait dans le meme état qu'au début
+    return
+
+def traitementAvantP(monBut): #mon but est de la forme fait(valeur, autre)
+
+    regleTrouver = False # variable pour savoir si on trouve encore une règle
+    print(monBut)
+    if checkContrainte(monBut) : #si le but est dans les faits alors il est atteind
+        print("but atteind dans la base de fait")
+        return 2
+    else :
+
+        for regles,conditions in BR.items() : #pour chaque regles
+            if regleTrouver == False :
+                print("pour chaque règle")
+                regle = regles.split(",") #on met les conséquences sous forme de liste (avant ->)
+        
+                variables = []
+                faits = []        
+
+                for groupeCondition in conditions:#pour chaque conditions (après ->)
+                    print("pour chaque conditions")
+                    indiceVariable = 0
+                    for condition in groupeCondition: # on sépare les variables des conditions
+                        if indiceVariable < len(regle): #si ce sont des variables on stock dans variables
+                            variables.append(condition)
+                            print("liste de variable")
+                        else :
+                            faits.append(condition) #si ce sont des conditions on stock dans faits
+                            print("liste de faits a vérifier")
+                        indiceVariable += 1
+
+
+                #il faut voir si les faits correspondent au but
+                for elements in faits : #pour chaque règle qui amène a cette conséquence
+                    print("cet ensemble de faits a vérifier")
+                    print(elements)
+                    valider = True
+                    for element in elements: # element ressemble a : fait(valeur)
+                         #on regarde si chaque condition, elle est vraie dans la base de faits
+
+                        if(valider == True) :
+                            fait = re.compile(r'(?P<fait>[\!\w]+)\((?P<valeur>[\s\w\-]+(,[\s\w\-]+)*)\)\s*')
+                            monFait = re.match(fait,element)   
+                            if monFait is not None :
+
+                                fait = re.compile(r'(?P<fait>[\!\w]+)\((?P<valeur>[\s\w\-]+(,[\s\w\-]+)*)\)\s*')
+                                monFait = re.match(fait,element)   
+                                if not(checkContrainte(element)): #s'il n'y est pas on invalide cette règle
+                                    print("pas dans la base")
+                                    valider = False
+                                else :
+                                    print("dans la base")
+
+           
+                    if valider == True : # si on a passer toutes les contraintes et qu'elles sont toutes dans la base de faits 
+                        print("cette règle fonctionne")
+                        # il faut vérifier que le fait n'est pas déjà dans la base
+                        indice = 1 #indice pour retrouver les variables
+                        for nouveauFait in regle:
+                            if nouveauFait in BF :
+                                if variables[indice] in BF[nouveauFait] :
+                                #si le fait existe deja dans la base
+                                    print("le fait existe déjà")
+                                    print(nouveauFait)
+                                    print(variables[indice])
+                                else : # pour ce fait la valeur n'existe pas encore dans la base
+                                    regleTrouver = True
+                                    addFait(nouveauFait,variables[indice])
+                                    print("on ajoute le fait" + nouveauFait) 
+                                    print(variables[indice]) 
+
+                            else : # le fait n'xiste pas encore dans la base
+                                regleTrouver = True
+                                addFait(nouveauFait,variables[indice])
+                                print("on ajoute le fait" + nouveauFait)
+                                print(variables[indice] ) 
+
+                            indice += 1           
+        
+
+    if regleTrouver == False :
+        return 0
+    else :
+        return traitementAvantP(monBut)
+    return -1
+
+
 
 def checkContradiction(pile):
 	for elem in pile:
